967480b96695f15f7a93def29fe324ec32d66014
shrikant <shrikant.pm14@gmail.com>
2023-06-01 11:39:26 -0500

    added openplugin as library

--- permutate/runner.py
+++ permutate/runner.py
@@ -4,6 +4,7 @@ import webbrowser
 from tqdm import tqdm
 from .logger import logger
 from datetime import datetime
+from openplugin import run_plugin_selector
 from .job_request_schema import JobRequest
 from .job_response_schema import JobResponse, JobSummary, JobDetail
 
@@ -65,12 +66,6 @@ class Runner:
 
     @staticmethod
     def run_single_permutation_test_case(test_case, config, permutation, plugin_group, permutation_summary):
-        if permutation.tool_selector.get("provider") == "Imprompt":
-            url = config.imprompt_tool_selector
-        elif permutation.tool_selector.get("provider") == "Langchain":
-            url = config.langchain_tool_selector
-        else:
-            raise Exception("Tool selector provider not supported")
         payload = json.dumps({
             "messages": [{
                 "content": test_case.prompt,
@@ -81,58 +76,26 @@ class Runner:
             "tool_selector_config": permutation.tool_selector,
             "llm": permutation.llm
         })
-        headers = {'Content-Type': 'application/json'}
-        response = requests.request("POST", url, headers=headers, data=payload)
-        if response.status_code == 200:
-            response_json = response.json()
-            is_plugin_detected = False
-            is_plugin_operation_found = False
-            is_plugin_parameter_mapped = False
-            parameter_mapped_percentage = 0
-            plugin_operation = None
-            plugin_name = None
-            plugin_parameters_mapped = None
-            for detected_plugin_operation in response_json.get("detected_plugin_operations"):
-                if detected_plugin_operation.get("plugin").get("name_for_model") == test_case.expected_plugin_used or \
-                        detected_plugin_operation.get("plugin").get("name_for_human") == test_case.expected_plugin_used:
-                    is_plugin_detected = True
-                    plugin_name = detected_plugin_operation.get("plugin").get("name_for_human")
-                    if detected_plugin_operation.get("plugin").get("api_called") == test_case.expected_api_used:
-                        is_plugin_operation_found = True
-                    plugin_operation = detected_plugin_operation.get("plugin").get("api_called")
-                    plugin_parameters_mapped = detected_plugin_operation.get("mapped_operation_parameters")
-                    if plugin_parameters_mapped:
-                        expected_params = test_case.expected_parameters
-                        common_pairs = {k: plugin_parameters_mapped[k] for k in plugin_parameters_mapped if
-                                        k in expected_params and plugin_parameters_mapped[k] == expected_params[k]}
-                        if len(common_pairs) == len(expected_params):
-                            parameter_mapped_percentage = 100
-                            is_plugin_parameter_mapped = True
-                        else:
-                            parameter_mapped_percentage = len(common_pairs) / len(expected_params) * 100
-
-            detail = JobDetail(
-                permutation_name=permutation.name,
-                permutation_summary=permutation_summary,
-                test_case_name=test_case.name,
-                is_run_completed=True,
-                language="English",
-                prompt=test_case.prompt,
-                final_output=response_json.get("final_text_response"),
-                match_score="0.0",
-                is_plugin_detected=is_plugin_detected,
-                is_plugin_operation_found=is_plugin_operation_found,
-                is_plugin_parameter_mapped=is_plugin_parameter_mapped,
-                parameter_mapped_percentage=parameter_mapped_percentage,
-                plugin_name=plugin_name,
-                plugin_operation=plugin_operation,
-                plugin_parameters_mapped=plugin_parameters_mapped,
-                response_time_sec=response_json.get("response_time"),
-                total_llm_tokens_used=response_json.get("tokens_used"),
-                llm_api_cost=response_json.get("llm_api_cost")
-            )
+        passed = True
+        if config.use_openplugin_library:
+            response_json = run_plugin_selector(payload)
+            if response_json is None:
+                passed = False
         else:
-            detail = JobDetail(
+            if permutation.tool_selector.get("provider") == "Imprompt":
+                url = config.imprompt_tool_selector
+            elif permutation.tool_selector.get("provider") == "Langchain":
+                url = config.langchain_tool_selector
+            else:
+                raise Exception("Tool selector provider not supported")
+            headers = {'Content-Type': 'application/json'}
+            response = requests.request("POST", url, headers=headers, data=payload)
+            if response.status_code != 200:
+                passed = False
+            response_json = response.json()
+
+        if not passed or response_json is None:
+            return JobDetail(
                 permutation_name=permutation.name,
                 permutation_summary=permutation_summary,
                 test_case_name=test_case.name,
@@ -149,4 +112,51 @@ class Runner:
                 total_llm_tokens_used=0,
                 llm_api_cost=0
             )
+
+        is_plugin_detected = False
+        is_plugin_operation_found = False
+        is_plugin_parameter_mapped = False
+        parameter_mapped_percentage = 0
+        plugin_operation = None
+        plugin_name = None
+        plugin_parameters_mapped = None
+        for detected_plugin_operation in response_json.get("detected_plugin_operations"):
+            if detected_plugin_operation.get("plugin").get("name_for_model") == test_case.expected_plugin_used or \
+                    detected_plugin_operation.get("plugin").get("name_for_human") == test_case.expected_plugin_used:
+                is_plugin_detected = True
+                plugin_name = detected_plugin_operation.get("plugin").get("name_for_human")
+                if detected_plugin_operation.get("plugin").get("api_called") == test_case.expected_api_used:
+                    is_plugin_operation_found = True
+                plugin_operation = detected_plugin_operation.get("plugin").get("api_called")
+                plugin_parameters_mapped = detected_plugin_operation.get("mapped_operation_parameters")
+                if plugin_parameters_mapped:
+                    expected_params = test_case.expected_parameters
+                    common_pairs = {k: plugin_parameters_mapped[k] for k in plugin_parameters_mapped if
+                                    k in expected_params and plugin_parameters_mapped[k] == expected_params[k]}
+                    if len(common_pairs) == len(expected_params):
+                        parameter_mapped_percentage = 100
+                        is_plugin_parameter_mapped = True
+                    else:
+                        parameter_mapped_percentage = len(common_pairs) / len(expected_params) * 100
+
+        detail = JobDetail(
+            permutation_name=permutation.name,
+            permutation_summary=permutation_summary,
+            test_case_name=test_case.name,
+            is_run_completed=True,
+            language="English",
+            prompt=test_case.prompt,
+            final_output=response_json.get("final_text_response"),
+            match_score="0.0",
+            is_plugin_detected=is_plugin_detected,
+            is_plugin_operation_found=is_plugin_operation_found,
+            is_plugin_parameter_mapped=is_plugin_parameter_mapped,
+            parameter_mapped_percentage=parameter_mapped_percentage,
+            plugin_name=plugin_name,
+            plugin_operation=plugin_operation,
+            plugin_parameters_mapped=plugin_parameters_mapped,
+            response_time_sec=response_json.get("response_time"),
+            total_llm_tokens_used=response_json.get("tokens_used"),
+            llm_api_cost=response_json.get("llm_api_cost")
+        )
         return detail
